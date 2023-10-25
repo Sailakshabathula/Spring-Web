822273c99ec33e4d977c726ad9b93c785d1508a7
sundar <sai953392@gmail.com>
2023-10-25 21:02:10 +0530

    add developing branch

--- src/main/java/com/example/demo/Springweb2Application.java
+++ src/main/java/com/example/demo/Springweb2Application.java
@@ -8,6 +8,12 @@ public class Springweb2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springweb2Application.class, args);
+
+		//this changes taken from development branch.
+
+		//this development branch i am try to merge into the main branch
+
+		System.out.println("welcome");
 	}
 
 }
